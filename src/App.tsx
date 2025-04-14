cdc4a5b38b5d14472b99d46ff806963a8fda4a01
김지훈 <kigpand@gmail.com>
2025-04-14 23:59:00 +0900

    feat: 버튼 install

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
-  return <div className="w-screen h-screen bg-pink-100">tailwindcss</div>;
+  return (
+    <div className="w-screen h-screen bg-pink-100">
+      <Button variant="secondary">크와앙</Button>
+    </div>
+  );
 }
 
 export default App;
