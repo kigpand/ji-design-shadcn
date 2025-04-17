70f0ac6d82222601880a6cbbc0fd27bfdfeb2b0a
김지훈 <kigpand@gmail.com>
2025-04-17 22:45:49 +0900

    refact: 컴포넌트 구조 변경

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,5 +1,5 @@
+import { Button } from "@/components";
 import type { Meta, StoryObj } from "@storybook/react";
-import { Button } from "@/components/ui/button";
 
 const meta: Meta<typeof Button> = {
   title: "Design System/Button",
