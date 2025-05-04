ce7ce37f4f6991caa629841433f40e8986f104ac
김지훈 <kigpand@gmail.com>
2025-05-04 21:34:08 +0900

    feat: storybook testing 기능 추가

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -1,5 +1,6 @@
 import { Input } from "@/components";
 import type { Meta, StoryObj } from "@storybook/react";
+import { userEvent, within } from "@storybook/testing-library";
 
 const meta: Meta<typeof Input> = {
   title: "Components/Input",
@@ -40,5 +41,20 @@ export const Disabled: Story = {
   args: {
     disabled: true,
     placeholder: "비활성화된 인풋",
+  },
+};
+
+export const TypingInteraction: Story = {
+  args: {
+    placeholder: "placeholder",
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const input = await canvas.getByPlaceholderText("placeholder");
+
+    await userEvent.type(input, "test");
+
+    expect(input).toHaveValue("test");
   },
 };
